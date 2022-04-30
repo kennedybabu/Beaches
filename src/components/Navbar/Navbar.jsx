d50a371d74de404c258699eed0603e4cffaaf1e4
kennedybabu <kennedynjogu8@gmail.com>
2022-04-30 12:54:42 +0300

    install react-icons library

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className="logo">
+            <h2>BEACHES.</h2>
+            
+            <ul className="nav-menu">
+                <li>Home</li>
+                <li>Destinations</li>
+                <li>Travel</li>
+                <li>Book</li>
+                <li>Views</li>
+            </ul>
+
+            <div className="nav-icons">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
